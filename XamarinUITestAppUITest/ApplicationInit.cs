55f8a705732c64a35ede78ceb63819f753fcbe6e
Craig Johnson <cjohnson@Craigs-MacBook-Pro-2.local>
2018-07-31 22:08:39 -0400

    initial posting of the sample Xamarin UITest solution that demonstrates the runtime error with the System.Runtime.Remoting assembly.

---
+++ XamarinUITestAppUITest/ApplicationInit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.UITest;
+
+namespace XamarinUITestAppUITest
+{
+    public static class ApplicationInit
+    {
+        public static IApp AppInit(Platform platform)
+        {
+            if (platform == Platform.iOS)
+            {
+                return ConfigureApp
+                    .iOS
+                    .PreferIdeSettings()
+                    .AppBundle("../XamarinUITestApp/bin/iPhoneSimulator/Debug/XamarinUITestApp.exe")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .Android
+                .PreferIdeSettings()
+                .StartApp();
+
+        }
+    }
+}
