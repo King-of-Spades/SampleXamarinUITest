28f550ce483a8e845213bdf5f87d2442a01db23b
Craig Johnson <cjohnson@Craigs-MacBook-Pro-2.local>
2018-08-06 22:09:14 -0400

    Removed the unit test app from the project.  It appears that it doesn't support testing the UI.  Added a unit test project and forced it's inclusion by manually changing the project file.

---
+++ XamarinUITestAppTest/Helpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.UITest;
+
+namespace XamarinUITestAppTest
+{
+    public static class Helpers
+    {
+        public static IApp StartSim()
+        {
+            return ConfigureApp.iOS.DeviceIdentifier("B52C52D7-601C-4A72-8983-FB3E4C5AEEBD").StartApp();
+        }
+    }
+}
